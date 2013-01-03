679c11a430f5a4796950767edb3826e3156f461d
François Frisch <francoisfrisch@gmail.com>
2013-01-02 17:20:09 -0800

    Initial Commit

---
+++ run.js
@@ -0,0 +1,20 @@
+var Promise = require("montage/core/promise").Promise;
+
+exports.run = function( suiteRequire, modules ) {
+
+    var spec = queryString("spec");
+    if (spec) {
+        suiteRequire.async(decodeURIComponent(spec)).then(function() {
+            jasmine.getEnv().execute();
+        }).done();
+    } else {
+        Promise.all(modules.map(suiteRequire.deepLoad))
+        .then(function () {
+            modules.forEach(suiteRequire);
+            jasmine.getEnv().execute();
+        }).then(function() {
+                console.warn("loaded");
+            window.__testacular__.loaded()
+        });
+    }
+}
